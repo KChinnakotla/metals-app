90d84abd253bedf41a7f4aca59b3e4e19d351c89
Keshav Chinnakotla <keshav.chinnakotla@gmail.com>
2024-09-13 11:41:40 -0400

    Create metalsList.py

---
+++ src/metalsList.py
@@ -0,0 +1,14 @@
+#creates a list of Alkali metals and a variable for display
+alkaliList = ['Li','Na','K','Rb','Cs','Fr']
+alkaliDisplay = Label(alkaliList[0],200,200,size = 100,bold = True)
+alkaliDisplay.visible = False
+
+#creates a list of Alkaline metals and a variable for display
+alkalineList = ['Be','Mg','Ca','Sr','Ba','Ra']
+alkalineDisplay = Label(alkalineList[0],200,200,size = 100,bold = True)
+alkalineDisplay.visible = False
+
+#creates a list of certain transition metals and a variable for display
+otherList = ['Au','Ag','Ni']
+otherDisplay = Label(otherList[0],200,200,size = 100,bold = True)
+otherDisplay.visible = False
